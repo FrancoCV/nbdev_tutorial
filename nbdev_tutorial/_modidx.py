63a13db16c37ede46fdf6860560e7ef04bb24586
francocv95 <fcometto@singular4.com>
2023-02-15 16:33:05 -0300

    Add `say_hello`; update index

--- nbdev_tutorial/_modidx.py
+++ nbdev_tutorial/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://FrancoCV.github.io',
                 'git_url': 'https://github.com/FrancoCV/nbdev_tutorial',
                 'lib_path': 'nbdev_tutorial'},
-  'syms': {'nbdev_tutorial.core': {'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py')}}}
+  'syms': { 'nbdev_tutorial.core': { 'nbdev_tutorial.core.foo': ('core.html#foo', 'nbdev_tutorial/core.py'),
+                                     'nbdev_tutorial.core.say_hello': ('core.html#say_hello', 'nbdev_tutorial/core.py')}}}
